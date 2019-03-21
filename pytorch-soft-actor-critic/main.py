ba3bb3a0b9bec4d011e0f903aa48ddd763de2e54
joeybose <joey.bose@mail.mcgill.ca>
2019-03-21 14:11:35 -0400

    Soft Actor Critic New Repo

---
+++ pytorch-soft-actor-critic/main.py
@@ -0,0 +1,185 @@
+import argparse
+import os
+import time
+from comet_ml import Experiment
+import json
+import gym
+from tqdm import tqdm
+import numpy as np
+import itertools
+import torch
+import ipdb
+from sac import SAC
+from tensorboardX import SummaryWriter
+from normalized_actions import NormalizedActions
+from replay_memory import ReplayMemory
+
+def main(args):
+    # Environment
+    env = NormalizedActions(gym.make(args.env_name))
+    env.seed(args.seed)
+    torch.manual_seed(args.seed)
+    np.random.seed(args.seed)
+
+    # Agent
+    agent = SAC(env.observation_space.shape[0], env.action_space, args)
+
+    writer = SummaryWriter()
+
+    # Memory
+    memory = ReplayMemory(args.replay_size)
+
+    # Training Loop
+    rewards = []
+    test_rewards = []
+    total_numsteps = 0
+    updates = 0
+
+    if args.debug:
+        args.use_logger = False
+        ipdb.set_trace()
+
+        # Check if settings file
+    if os.path.isfile("settings.json"):
+        with open('settings.json') as f:
+                data = json.load(f)
+        args.comet_apikey = data["apikey"]
+        args.comet_username = data["username"]
+    else:
+        raise NotImplementedError
+
+    if args.comet:
+        experiment = Experiment(api_key=args.comet_apikey,\
+        project_name="florl",auto_output_logging="None",\
+        workspace=args.comet_username,auto_metric_logging=False,\
+        auto_param_logging=False)
+        experiment.set_name(args.namestr)
+        args.experiment = experiment
+
+    for i_episode in itertools.count():
+        state = env.reset()
+
+        episode_reward = 0
+        while True:
+            if args.start_steps > total_numsteps:
+                action = env.action_space.sample()
+            else:
+                action = agent.select_action(state)  # Sample action from policy
+            next_state, reward, done, _ = env.step(action)  # Step
+            mask = not done  # 1 for not done and 0 for done
+            memory.push(state, action, reward, next_state, mask)  # Append transition to memory
+            if len(memory) > args.batch_size:
+                for i in range(args.updates_per_step): # Number of updates per step in environment
+                    # Sample a batch from memory
+                    state_batch, action_batch, reward_batch, next_state_batch,\
+                            mask_batch = memory.sample(args.batch_size)
+                    # Update parameters of all the networks
+                    value_loss, critic_1_loss, critic_2_loss, policy_loss,\
+                            ent_loss, alpha = agent.update_parameters(state_batch,\
+                            action_batch,reward_batch,next_state_batch,mask_batch, updates)
+
+                    writer.add_scalar('loss/value', value_loss, updates)
+                    writer.add_scalar('loss/critic_1', critic_1_loss, updates)
+                    writer.add_scalar('loss/critic_2', critic_2_loss, updates)
+                    writer.add_scalar('loss/policy', policy_loss, updates)
+                    writer.add_scalar('loss/entropy_loss', ent_loss, updates)
+                    writer.add_scalar('entropy_temprature/alpha', alpha, updates)
+                    if args.comet:
+                        args.experiment.log_metric("Loss Value", value_loss,step=updates)
+                        args.experiment.log_metric("Loss Critic 1",critic_1_loss,step=updates)
+                        args.experiment.log_metric("Loss Critic 2",critic_2_loss,step=updates)
+                        args.experiment.log_metric("Loss Policy",policy_loss,step=updates)
+                        args.experiment.log_metric("Loss Entropy",ent_loss,step=updates)
+                        args.experiment.log_metric("Entropy Temperature",alpha,step=updates)
+                    updates += 1
+
+            state = next_state
+            total_numsteps += 1
+            episode_reward += reward
+
+            if done:
+                break
+
+        if total_numsteps > args.num_steps:
+            break
+
+        writer.add_scalar('reward/train', episode_reward, i_episode)
+        rewards.append(episode_reward)
+        if args.comet:
+            args.experiment.log_metric("Train Reward",episode_reward,step=i_episode)
+            args.experiment.log_metric("Average Train Reward",\
+                    np.round(np.mean(rewards[-100:]),2),step=i_episode)
+        print("Episode: {}, total numsteps: {}, reward: {}, average reward: {}".format(i_episode,\
+                        total_numsteps, np.round(rewards[-1],2),\
+                        np.round(np.mean(rewards[-100:]),2)))
+
+        if i_episode % 10 == 0 and args.eval == True:
+            state = torch.Tensor([env.reset()])
+            episode_reward = 0
+            while True:
+                action = agent.select_action(state, eval=True)
+
+                next_state, reward, done, _ = env.step(action)
+                episode_reward += reward
+
+
+                state = next_state
+                if done:
+                    break
+
+            writer.add_scalar('reward/test', episode_reward, i_episode)
+            if args.comet:
+                args.experiment.log_metric("Test Reward",episode_reward,step=i_episode)
+
+            test_rewards.append(episode_reward)
+            print("----------------------------------------")
+            print("Test Episode: {}, reward: {}".format(i_episode, test_rewards[-1]))
+            print("----------------------------------------")
+
+    env.close()
+
+if __name__ == '__main__':
+    """
+    Process command-line arguments, then call main()
+    """
+    parser = argparse.ArgumentParser(description='PyTorch REINFORCE example')
+    parser.add_argument('--env-name', default="HalfCheetah-v2",
+                        help='name of the environment to run')
+    parser.add_argument('--policy', default="Gaussian",
+                        help='algorithm to use: Gaussian | Deterministic')
+    parser.add_argument('--eval', type=bool, default=True,
+                        help='Evaluates a policy a policy every 10 episode (default:True)')
+    parser.add_argument('--gamma', type=float, default=0.99, metavar='G',
+                        help='discount factor for reward (default: 0.99)')
+    parser.add_argument('--tau', type=float, default=0.005, metavar='G',
+                        help='target smoothing coefficient(τ) (default: 0.005)')
+    parser.add_argument('--lr', type=float, default=0.0003, metavar='G',
+                        help='learning rate (default: 0.0003)')
+    parser.add_argument('--alpha', type=float, default=0.1, metavar='G',
+                        help='Temperature parameter α determines the relative importance of the entropy term against the reward (default: 0.1)')
+    parser.add_argument('--automatic_entropy_tuning', type=bool, default=False, metavar='G',
+                        help='Temperature parameter α automaically adjusted.')
+    parser.add_argument('--seed', type=int, default=456, metavar='N',
+                        help='random seed (default: 456)')
+    parser.add_argument('--batch_size', type=int, default=1024, metavar='N',
+                        help='batch size (default: 256)')
+    parser.add_argument('--num_steps', type=int, default=1000001, metavar='N',
+                        help='maximum number of steps (default: 1000000)')
+    parser.add_argument('--hidden_size', type=int, default=256, metavar='N',
+                        help='hidden size (default: 256)')
+    parser.add_argument('--updates_per_step', type=int, default=1, metavar='N',
+                        help='model updates per simulator step (default: 1)')
+    parser.add_argument('--start_steps', type=int, default=10000, metavar='N',
+                        help='Steps sampling random actions (default: 10000)')
+    parser.add_argument('--target_update_interval', type=int, default=1, metavar='N',
+                        help='Value target update per no. of updates per step (default: 1)')
+    parser.add_argument('--replay_size', type=int, default=1000000, metavar='N',
+                        help='size of replay buffer (default: 10000000)')
+    parser.add_argument("--comet", action="store_true", default=False,help='Use comet for logging')
+    parser.add_argument('--debug', default=False, action='store_true',help='Debug')
+    parser.add_argument('--namestr', type=str, default='FloRL', \
+            help='additional info in output filename to describe experiments')
+
+    args = parser.parse_args()
+    main(args)
+
---
+++ pytorch-soft-actor-critic/model.py
@@ -0,0 +1,123 @@
+import sys
+import os
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.distributions import Normal
+
+LOG_SIG_MAX = 2
+LOG_SIG_MIN = -20
+epsilon = 1e-6
+
+# Initialize Policy weights
+def weights_init_(m):
+    classname = m.__class__.__name__
+    if classname.find('Linear') != -1:
+        torch.nn.init.xavier_uniform_(m.weight, gain=1)
+        torch.nn.init.constant_(m.bias, 0)
+
+
+class ValueNetwork(nn.Module):
+    def __init__(self, num_inputs, hidden_dim):
+        super(ValueNetwork, self).__init__()
+
+        self.linear1 = nn.Linear(num_inputs, hidden_dim)
+        self.linear2 = nn.Linear(hidden_dim, hidden_dim)
+        self.linear3 = nn.Linear(hidden_dim, 1)
+
+        self.apply(weights_init_)
+
+    def forward(self, state):
+        x = F.relu(self.linear1(state))
+        x = F.relu(self.linear2(x))
+        x = self.linear3(x)
+        return x
+
+
+class QNetwork(nn.Module):
+    def __init__(self, num_inputs, num_actions, hidden_dim):
+        super(QNetwork, self).__init__()
+
+        # Q1 architecture
+        self.linear1 = nn.Linear(num_inputs + num_actions, hidden_dim)
+        self.linear2 = nn.Linear(hidden_dim, hidden_dim)
+        self.linear3 = nn.Linear(hidden_dim, 1)
+
+        # Q2 architecture
+        self.linear4 = nn.Linear(num_inputs + num_actions, hidden_dim)
+        self.linear5 = nn.Linear(hidden_dim, hidden_dim)
+        self.linear6 = nn.Linear(hidden_dim, 1)
+
+        self.apply(weights_init_)
+
+    def forward(self, state, action):
+        x1 = torch.cat([state, action], 1)
+        x1 = F.relu(self.linear1(x1))
+        x1 = F.relu(self.linear2(x1))
+        x1 = self.linear3(x1)
+
+        x2 = torch.cat([state, action], 1)
+        x2 = F.relu(self.linear4(x2))
+        x2 = F.relu(self.linear5(x2))
+        x2 = self.linear6(x2)
+
+        return x1, x2
+
+
+class GaussianPolicy(nn.Module):
+    def __init__(self, num_inputs, num_actions, hidden_dim):
+        super(GaussianPolicy, self).__init__()
+
+        self.linear1 = nn.Linear(num_inputs, hidden_dim)
+        self.linear2 = nn.Linear(hidden_dim, hidden_dim)
+
+        self.mean_linear = nn.Linear(hidden_dim, num_actions)
+        self.log_std_linear = nn.Linear(hidden_dim, num_actions)
+
+        self.apply(weights_init_)
+
+    def forward(self, state):
+        x = F.relu(self.linear1(state))
+        x = F.relu(self.linear2(x))
+        mean = self.mean_linear(x)
+        log_std = self.log_std_linear(x)
+        log_std = torch.clamp(log_std, min=LOG_SIG_MIN, max=LOG_SIG_MAX)
+        return mean, log_std
+
+    def sample(self, state):
+        mean, log_std = self.forward(state)
+        std = log_std.exp()
+        normal = Normal(mean, std)
+        x_t = normal.rsample()  # for reparameterization trick (mean + std * N(0,1))
+        action = torch.tanh(x_t)
+        log_prob = normal.log_prob(x_t)
+        # Enforcing Action Bound
+        log_prob -= torch.log(1 - action.pow(2) + epsilon)
+        log_prob = log_prob.sum(1, keepdim=True)
+        return action, log_prob, x_t, mean, log_std
+
+class DeterministicPolicy(nn.Module):
+    def __init__(self, num_inputs, num_actions, hidden_dim):
+        super(DeterministicPolicy, self).__init__()
+        self.linear1 = nn.Linear(num_inputs, hidden_dim)
+        self.linear2 = nn.Linear(hidden_dim, hidden_dim)
+
+        self.mean = nn.Linear(hidden_dim, num_actions)
+        self.noise = torch.Tensor(num_actions)
+
+        self.apply(weights_init_)
+
+    def forward(self, state):
+        x = F.relu(self.linear1(state))
+        x = F.relu(self.linear2(x))
+        mean = torch.tanh(self.mean(x))
+        return mean
+
+
+    def sample(self, state):
+        mean = self.forward(state)
+        noise = self.noise.normal_(0., std=0.1)
+        noise = noise.clamp(-0.25, 0.25)
+        action = mean + noise
+        return action, torch.tensor(0.), torch.tensor(0.), mean, torch.tensor(0.)
+    
