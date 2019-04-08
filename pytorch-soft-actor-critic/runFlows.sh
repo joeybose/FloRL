faaaad5564dcd4a520cd305fc5c0c515eef872de
joeybose <joey.bose@mail.mcgill.ca>
2019-04-08 11:14:35 -0400

    updated bash script

--- pytorch-soft-actor-critic/runFlows.sh
+++ pytorch-soft-actor-critic/runFlows.sh
@@ -7,7 +7,12 @@
 # Run the following scripts in parallel:
 
 # HyperParam Sweep
-COMET_DISABLE_AUTO_LOGGING=1 python main.py --namestr="Planar 3 Flow Lunar Lander" --env-name LunarLanderContinuous-v2 --batch_size=1024 --policy='Flow' --flow_model='planar' --n_blocks=3 --comet --seed=1 &
-COMET_DISABLE_AUTO_LOGGING=1 python main.py --namestr="Planar 5 Flow Lunar Lander" --env-name LunarLanderContinuous-v2 --batch_size=1024 --policy='Flow' --flow_model='planar' --n_blocks=5 --comet --seed=2 &
-COMET_DISABLE_AUTO_LOGGING=1 python main.py --namestr="Planar 7 Flow Lunar Lander" --env-name LunarLanderContinuous-v2 --batch_size=1024 --policy='Flow' --flow_model='planar' --n_blocks=7 --comet --seed=3 &
-COMET_DISABLE_AUTO_LOGGING=1 python main.py --namestr="Planar 9 Flow Lunar Lander" --env-name LunarLanderContinuous-v2 --batch_size=1024 --policy='Flow' --flow_model='planar' --n_blocks=9 --comet --seed=4&
+#COMET_DISABLE_AUTO_LOGGING=1 python main.py --namestr="Planar 3 Flow Lunar Lander" --env-name LunarLanderContinuous-v2 --batch_size=1024 --policy='Flow' --flow_model='planar' --n_blocks=3 --comet --seed=1 &
+#COMET_DISABLE_AUTO_LOGGING=1 python main.py --namestr="Planar 5 Flow Lunar Lander" --env-name LunarLanderContinuous-v2 --batch_size=1024 --policy='Flow' --flow_model='planar' --n_blocks=5 --comet --seed=2 &
+#COMET_DISABLE_AUTO_LOGGING=1 python main.py --namestr="Planar 7 Flow Lunar Lander" --env-name LunarLanderContinuous-v2 --batch_size=1024 --policy='Flow' --flow_model='planar' --n_blocks=7 --comet --seed=3 &
+#COMET_DISABLE_AUTO_LOGGING=1 python main.py --namestr="Planar 9 Flow Lunar Lander" --env-name LunarLanderContinuous-v2 --batch_size=1024 --policy='Flow' --flow_model='planar' --n_blocks=9 --comet --seed=4 &
+
+COMET_DISABLE_AUTO_LOGGING=1 python main.py --namestr="Planar 3 MENT Flow Pendulum" --env-name Pendulum-v0 --batch_size=1024 --policy='Flow' --flow_model='planar' --n_blocks=3 --comet --seed=1 --mean_entropy&
+COMET_DISABLE_AUTO_LOGGING=1 python main.py --namestr="Planar 5 MENT Flow Pendulum" --env-name Pendulum-v0 --batch_size=1024 --policy='Flow' --flow_model='planar' --n_blocks=5 --comet --seed=2 --mean_entropy &
+COMET_DISABLE_AUTO_LOGGING=1 python main.py --namestr="Planar 7 MENT Flow Pendulum" --env-name Pendulum-v0 --batch_size=1024 --policy='Flow' --flow_model='planar' --n_blocks=7 --comet --seed=3 --mean_entropy &
+COMET_DISABLE_AUTO_LOGGING=1 python main.py --namestr="Planar 9 MENT Flow Pendulum" --env-name Pendulum-v0 --batch_size=1024 --policy='Flow' --flow_model='planar' --n_blocks=9 --comet --seed=4 --mean_entropy&
