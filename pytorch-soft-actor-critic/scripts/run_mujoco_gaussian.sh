4f56cd5cdaa10450c949dffe8e2f53d69023769d
Ariella Smofsky <ariellasmofsky@gmail.com>
2019-04-23 13:23:07 -0400

    mujoco gaussian runscripts

---
+++ pytorch-soft-actor-critic/scripts/run_mujoco_gaussian.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -x
+
+#echo "Getting into the script"
+
+# Script to run multiple batches of experiments together.
+# Can also be used for different hyperparameter settings.
+
+# Run the following scripts in parallel:
+
+COMET_DISABLE_AUTO_LOGGING=1 python main.py --namestr=G-HC --env-name HalfCheetah-v1 --batch_size=1024 --policy=Gaussian --seed=0 --comet
