c7b5d15ee87c3fa89538ae146552af0a75f66826
Ariella Smofsky <ariellasmofsky@gmail.com>
2019-04-24 13:08:40 -0400

    Plots and scripts updates

---
+++ pytorch-soft-actor-critic/scripts/run_mujoco_exp.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -x
+
+#echo "Getting into the script"
+
+# Script to run multiple batches of experiments together.
+# Can also be used for different hyperparameter settings.
+
+# Run the following scripts in parallel:
+
+#COMET_DISABLE_AUTO_LOGGING=1 python main.py --namestr=G-HC --env-name HalfCheetah-v1 --batch_size=1024 --policy=Exponential --seed=0 --comet &
+#COMET_DISABLE_AUTO_LOGGING=1 python main.py --namestr=G-HC --env-name HalfCheetah-v1 --batch_size=1024 --policy=Exponential --seed=0 --comet &
+
+#COMET_DISABLE_AUTO_LOGGING=1 python main.py --namestr=G-NT-HC --env-name HalfCheetah-v1 --batch_size=1024 --policy=Exponential --seed=0 --tanh_off --comet &
+#COMET_DISABLE_AUTO_LOGGING=1 python main.py --namestr=G-NT-HC --env-name HalfCheetah-v1 --batch_size=1024 --policy=Exponential --seed=0 --tanh_off --comet &
+
+#python main.py --namestr=G-LL --env-name LunarLanderContinuous-v2 --batch_size=1024 --policy=Exponential --seed=3 --comet &
+#python main.py --namestr=G-LL --env-name LunarLanderContinuous-v2 --batch_size=1024 --policy=Exponential --seed=3 --comet
+
+#python main.py --namestr=G-NT-LL --env-name LunarLanderContinuous-v2 --batch_size=1024 --policy=Exponential --seed=3 --tanh_off --comet &
+#python main.py --namestr=G-NT-LL --env-name LunarLanderContinuous-v2 --batch_size=1024 --policy=Exponential --seed=3 --tanh_off --comet
