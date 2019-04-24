f43e580ab342a5aae9d59b0a0d0b14160dfe8bcc
Ariella <ariellasmofsky@gmail.com>
2019-04-24 01:26:08 -0400

    update run experiment scripts

--- pytorch-soft-actor-critic/scripts/run_contgridworld_gauss.sh
+++ pytorch-soft-actor-critic/scripts/run_contgridworld_gauss.sh
@@ -9,10 +9,13 @@ set -x
 
 # Run the following scripts in parallel:
 
-COMET_DISABLE_AUTO_LOGGING=1 python main.py --namestr=G-S-CG --make_cont_grid --batch_size=128 --silent --replay_size=100000 --hidden_size=64 --num_steps=100000 --policy=Gaussian --smol --comet &
+#COMET_DISABLE_AUTO_LOGGING=1 python main.py --namestr=G-S-CG --make_cont_grid --batch_size=128 --silent --replay_size=100000 --hidden_size=64 --num_steps=100000 --policy=Gaussian --smol --comet
 
-COMET_DISABLE_AUTO_LOGGING=1 python main.py --namestr=G-S-DG-CG --make_cont_grid --batch_size=128 --silent --replay_size=100000 --hidden_size=64 --num_steps=100000 --policy=Gaussian --smol --comet --dense_goals
+#COMET_DISABLE_AUTO_LOGGING=1 python main.py --namestr=G-S-DG-CG --make_cont_grid --batch_size=128 --replay_size=100000 --hidden_size=64 --num_steps=100000 --policy=Gaussian --smol --comet --dense_goals &
+#COMET_DISABLE_AUTO_LOGGING=1 python main.py --namestr=G-S-DG-CG --make_cont_grid --batch_size=128 --replay_size=100000 --hidden_size=64 --num_steps=100000 --policy=Gaussian --smol --comet --dense_goals
 
-COMET_DISABLE_AUTO_LOGGING=1 python main.py --namestr=G-NT-S-CG --make_cont_grid --batch_size=128 --silent --replay_size=100000 --hidden_size=64 --num_steps=100000 --policy=Gaussian --smol --comet &
-COMET_DISABLE_AUTO_LOGGING=1 python main.py --namestr=G-NT-S-CG --make_cont_grid --batch_size=128 --silent --replay_size=100000 --hidden_size=64 --num_steps=100000 --policy=Gaussian --smol --comet &
-COMET_DISABLE_AUTO_LOGGING=1 python main.py --namestr=G-NT-S-CG --make_cont_grid --batch_size=128 --silent --replay_size=100000 --hidden_size=64 --num_steps=100000 --policy=Gaussian --smol --comet &
+COMET_DISABLE_AUTO_LOGGING=1 python main.py --namestr=G-S-CG --make_cont_grid --batch_size=128 --replay_size=100000 --hidden_size=64 --num_steps=150000 --policy=Gaussian --smol --comet --alpha=0.1 &
+COMET_DISABLE_AUTO_LOGGING=1 python main.py --namestr=G-S-CG --make_cont_grid --batch_size=128 --replay_size=100000 --hidden_size=64 --num_steps=150000 --policy=Gaussian --smol --comet --alpha=0.2 &
+COMET_DISABLE_AUTO_LOGGING=1 python main.py --namestr=G-S-CG --make_cont_grid --batch_size=128 --replay_size=100000 --hidden_size=64 --num_steps=150000 --policy=Gaussian --smol --comet --alpha=0.3 &
+COMET_DISABLE_AUTO_LOGGING=1 python main.py --namestr=G-S-CG --make_cont_grid --batch_size=128 --replay_size=100000 --hidden_size=64 --num_steps=150000 --policy=Gaussian --smol --comet --alpha=0.4 &
+COMET_DISABLE_AUTO_LOGGING=1 python main.py --namestr=G-S-CG --make_cont_grid --batch_size=128 --replay_size=100000 --hidden_size=64 --num_steps=150000 --policy=Gaussian --smol --comet --alpha=0.5 &
